d5bd10d7918934a307c13f42d52e775d6d866678
Pouria Ezzati <ezzati.upt@gmail.com>
2018-06-24 10:10:47 +0430

    Configuration

--- next.config.js
+++ next.config.js
@@ -1,2 +1,4 @@
-const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+const withTypescript = require('@zeit/next-typescript');
+const withCSS = require('@zeit/next-css');
+
+module.exports = withTypescript(withCSS())
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
-import Link from 'next/link'
+import * as React from 'react';
+import Header from '../client/components/Header';
 
 export default () => (
-  <ul>
-    <li><Link href='/a' as='/a'><a>a</a></Link></li>
-    <li><Link href='/b' as='/b'><a>b</a></Link></li>
-  </ul>
-)
+  <Header />
+);
