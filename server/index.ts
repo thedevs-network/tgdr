c39c9baf43425880dea80a120c35a39e517f6ef5
Pouria Ezzati <ezzati.upt@gmail.com>
2018-06-24 10:10:55 +0430

    Set up basic server

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,41 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import * as next from 'next'
+import * as express from 'express';
+import * as next from 'next';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import * as helmet from 'helmet';
+import * as morgan from 'morgan';
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare()
-.then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+  .then(() => {
+    const server: express.Application = express();
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
+    // Use helmet to secure app with HTTP headers
+    server.use(helmet());
+
+    // Use morgan to log requests for dev
+    if (dev) {
+      server.use(morgan('dev'));
     }
-  })
-  .listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+
+    // Parse body and form data
+    server.use(cookieParser());
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({ extended: true }));
+
+    // Serve static files inside static folder
+    server.use(express.static('static'));
+
+    // Routes
+    server.get('*', (req: express.Request, res: express.Response) => handle(req, res));
+
+    // Start server
+    server.listen(port, err => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`); // tslint:disable-line:no-console
+    });
+  });
