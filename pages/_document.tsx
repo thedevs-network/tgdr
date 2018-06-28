d9a4453acc6369c5ede5caa4d8d80b18e12131cb
Pouria Ezzati <ezzati.upt@gmail.com>
2018-06-28 23:51:37 +0430

    Add default font "Rubik" to app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import styled, { ServerStyleSheet } from 'styled-components';
 import 'normalize.css';
 
 const Body = styled.body`
+  font: 16px/1.5 'Rubik', sans-serif;
   background-color: #F4F5F7;
 `;
 
@@ -20,6 +21,7 @@ class DocumentPage extends Document {
         <Head>
           <title>Telegram Directory | Find the best Telegram channels, bots and groups.</title>
           <link rel="stylesheet" href="/_next/static/style.css" />
+          <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,700" rel="stylesheet" />
           {this.props.styleTags}
         </Head>
         <Body>
