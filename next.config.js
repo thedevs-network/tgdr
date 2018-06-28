e54eb8b4f92dfacdbf585ee33761c29475dd3c3d
Pouria Ezzati <ezzati.upt@gmail.com>
2018-06-28 23:49:04 +0430

    Add importing images config for next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 const withTypescript = require('@zeit/next-typescript');
 const withCSS = require('@zeit/next-css');
+const withImages = require('next-images')
 
-module.exports = withTypescript(withCSS())
+module.exports = withTypescript(
+  withImages(
+    withCSS()
+  )
+)
