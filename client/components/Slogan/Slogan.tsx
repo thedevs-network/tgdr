060cac4bd8d0a4af473ccc0dc2d57a938011ba49
Pouria Ezzati <ezzati.upt@gmail.com>
2018-06-30 21:55:56 +0430

    Add slogan section

---
+++ client/components/Slogan/Slogan.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Flex } from 'grid-styled';
+
+const Title = styled.h2`
+  margin: 8px 0;
+  padding: 0;
+  font-size: 20px;
+  color: #808080;
+`;
+
+const SubTitle = styled.h3`
+  margin: 0;
+  padding: 0;
+  font-size: 16px;
+  font-weight: normal;
+  color: #A8A8A8;
+`;
+
+const Slogan: React.SFC = () => (
+  <Flex py={5} flexDirection="column" align="center">
+    <Title>
+      Discover the content you love on Telegram.
+    </Title>
+    <SubTitle>
+      Find top channels, bots and groups in Telegram.
+    </SubTitle>
+  </Flex>
+);
+
+export default Slogan;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import Header from '../client/components/Header';
+import Slogan from '../client/components/Slogan';
 
 export default () => (
-  <Header />
+  <>
+    <Header />
+    <Slogan />
+  </>
 );
