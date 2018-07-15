9cd4a4432d06ead7eab15fdc3e5b76301beeadd1
Pouria Ezzati <ezzati.upt@gmail.com>
2018-07-15 15:23:15 +0430

    Add typography components

---
+++ client/components/elements/Typography/Typography.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ChannelTitle = styled.h2`
+  margin: 0 0 12px;
+  padding: 0;
+  font-size: 26px;
+  font-weight: bold;
+  color: #666;
+`;
+
+export const ChannelDescription = styled.p`
+  margin: 0;
+  padding: 0;
+  font-size: 16px;
+  color: #808080;
+`;
