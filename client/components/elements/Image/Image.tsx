890c0a06555766201118afa406076e6d86eded59
Pouria Ezzati <ezzati.upt@gmail.com>
2018-07-15 15:23:37 +0430

    Add image component

---
+++ client/components/elements/Image/Image.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { Flex } from 'grid-styled';
+
+interface IImage {
+  h?: number | number[];
+  mb?: number | number[];
+  ml?: number | number[];
+  mr?: number | number[];
+  mt?: number | number[];
+  mx?: number | number[];
+  my?: number | number[];
+  w?: number | number[];
+}
+
+const ImageWrapper = styled(Flex) <IImage>`
+  height: ${({ h }) => h}px;
+  background-color: #eee;
+  border-radius: 50%;
+  overflow: hidden;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const Image: React.SFC<IImage> = ({ w, h, ...props }) => (
+  <ImageWrapper h={h} w={w} {...props}>
+    <Img src="" alt="" />
+  </ImageWrapper>
+);
+
+Image.defaultProps = {
+  h: 68,
+  w: 68,
+};
+
+export default Image;
