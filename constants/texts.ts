7aa52b09b3b921b7c7f450d2d5d11ff41a71a7ad
Pouria Ezzati <ezzati.upt@gmail.com>
2018-07-21 18:10:30 +0430

    Add submission info

---
+++ constants/texts.ts
@@ -0,0 +1,6 @@
+export const submissionInfo = [
+  'Submission must be in English language.',
+  "We don't accept anything related to adult contents.",
+  'We review and approve/reject submissions.',
+  'We might edit submissions info to be consistent.',
+];
