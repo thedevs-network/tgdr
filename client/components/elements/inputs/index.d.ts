8978b5341214330dea9195546ac3aa6f49e7e9d0
Pouria Ezzati <ezzati.upt@gmail.com>
2018-07-21 18:12:26 +0430

    Add input common interface

---
+++ client/components/elements/inputs/index.d.ts
@@ -0,0 +1,11 @@
+export interface IInput {
+  label: string;
+  mb?: number;
+  ml?: number;
+  mr?: number;
+  mt?: number;
+  mx?: number;
+  my?: number;
+  name: string;
+  placeholder?: string;
+};
