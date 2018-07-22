23c90af7391875df7287f1696127996ffafe0ab0
Pouria Ezzati <ezzati.upt@gmail.com>
2018-07-22 19:58:55 +0430

    Fix config property names

--- server/config.ts
+++ server/config.ts
@@ -3,14 +3,14 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 interface IConfig {
-  dbUri: string;
-  isDev: boolean;
+  db_uri: string;
+  is_dev: boolean;
   port: number;
 }
 
 const config: IConfig = {
-  dbUri: process.env.DB_URI || 'mongodb://localhost/tgdr',
-  isDev: process.env.NODE_ENV !== 'production',
+  db_uri: process.env.DB_URI || 'mongodb://localhost/tgdr',
+  is_dev: process.env.NODE_ENV !== 'production',
   port: parseInt(process.env.PORT, 10) || 3000,
 };
 
