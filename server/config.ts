97a023122a13827d5ec20688b65c91303ae2af42
Pouria Ezzati <ezzati.upt@gmail.com>
2018-07-31 20:45:51 +0430

    Add bot token to config

--- server/config.ts
+++ server/config.ts
@@ -3,13 +3,15 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 interface IConfig {
+  bot_token: string;
   db_uri: string;
   is_dev: boolean;
   port: number;
 }
 
 const config: IConfig = {
-  db_uri: process.env.DB_URI || 'mongodb://localhost/tgdr',
+  bot_token: process.env.BOT_TOKEN,
+  db_uri: process.env.DB_URI,
   is_dev: process.env.NODE_ENV !== 'production',
   port: parseInt(process.env.PORT, 10) || 3000,
 };
