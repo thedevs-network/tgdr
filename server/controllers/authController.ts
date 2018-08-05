883a835e78904c1026ad8b372f9f50470b845e94
Pouria Ezzati <ezzati.upt@gmail.com>
2018-08-05 13:37:34 +0430

    Add login handler

---
+++ server/controllers/authController.ts
@@ -0,0 +1,21 @@
+import * as express from 'express';
+import * as JWT from 'jsonwebtoken';
+import config from '../config';
+import { IUserModel } from '../models/User';
+
+/* Generate JWT */
+const signToken = (user: IUserModel) =>
+  JWT.sign(
+    {
+      exp: new Date().setDate(new Date().getDate() + 7),
+      iat: new Date().getTime(),
+      id: user.telegram_id,
+      name: user.first_name,
+    },
+    config.jwt_secret
+  );
+
+export const loginHandler = (req: express.Request, res: express.Response) => {
+  const jwt = signToken(req.user);
+  res.status(200).json({ token: jwt });
+};
