24167313a70b9b912da416b7b52003c52a71b1ed
Pouria Ezzati <ezzati.upt@gmail.com>
2018-08-05 13:37:45 +0430

    Use login handler

--- server/api/authApi.ts
+++ server/api/authApi.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
 import * as passport from 'passport';
+import { loginHandler } from '../controllers/authController';
 
 const router = express.Router();
 
 router.post(
   '/login',
-  passport.authenticate('telegram'));
+  passport.authenticate('telegram', { session: false }),
+  loginHandler);
 
 export default router;
