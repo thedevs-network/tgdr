c4f62e50a21cee40459e2514484d4fbd3cc9625b
Pouria Ezzati <ezzati.upt@gmail.com>
2018-08-05 13:37:23 +0430

    Add passport logic to login and save user

--- server/passport.ts
+++ server/passport.ts
@@ -1,9 +1,25 @@
 import * as passport from 'passport';
 import config from './config';
 import TelegramStratagy from 'passport-telegram-official';
+import User from './models/User';
 
 passport.use(new TelegramStratagy({
   botToken: config.bot_token,
-}, (profile, cb) => {
-  return cb(null, profile);
+}, async (profile, cb) => {
+  try {
+    const user = await User.findOneAndUpdate(
+      { telegram_id: profile.id },
+      {
+        first_name: profile.first_name.slice(0, 32),
+        telegram_id: profile.id,
+        username: profile.username
+      }, {
+        new: true,
+        upsert: true,
+      }
+    );
+    return cb(null, user);
+  } catch (error) {
+    return cb(error, false, "Couldn't authenticate.");
+  }
 }));
