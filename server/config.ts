d3971e24d59bb8f0b6f04ab6c166bebaa7d02e92
Pouria Ezzati <ezzati.upt@gmail.com>
2018-08-05 13:36:35 +0430

    Add jwt_secret to config

--- server/config.ts
+++ server/config.ts
@@ -6,6 +6,7 @@ interface IConfig {
   bot_token: string;
   db_uri: string;
   is_dev: boolean;
+  jwt_secret: string;
   port: number;
 }
 
@@ -13,6 +14,7 @@ const config: IConfig = {
   bot_token: process.env.BOT_TOKEN,
   db_uri: process.env.DB_URI,
   is_dev: process.env.NODE_ENV !== 'production',
+  jwt_secret: process.env.JWT_SECRET,
   port: parseInt(process.env.PORT, 10) || 3000,
 };
 
