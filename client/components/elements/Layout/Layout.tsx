b81a05dfdef463c06733fa87f88e40b4ca54f1f1
Pouria Ezzati <ezzati.upt@gmail.com>
2018-08-18 16:15:20 +0430

    Add app loading wrapper

--- client/components/elements/Layout/Layout.tsx
+++ client/components/elements/Layout/Layout.tsx
@@ -1,6 +1,20 @@
 import styled from 'styled-components';
 import { Flex } from 'grid-styled';
 
+export const AppLoadingWrapper = styled(Flex).attrs({
+  align: 'center',
+  flex: '1 1 auto',
+  justify: 'center',
+})`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
 export const Container = styled(Flex).attrs({
   px: 15,
   w: [540, 720, 960, 1140],
