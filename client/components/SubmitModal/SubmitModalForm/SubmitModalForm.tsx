50cff6a81b066700d784f2bdf3ae8d57424abe59
Pouria Ezzati <ezzati.upt@gmail.com>
2018-08-19 15:03:58 +0430

    Remove photo and type fields from submit form

--- client/components/SubmitModal/SubmitModalForm/SubmitModalForm.tsx
+++ client/components/SubmitModal/SubmitModalForm/SubmitModalForm.tsx
@@ -2,11 +2,10 @@ import * as React from 'react';
 import { Flex } from 'grid-styled';
 import Button from '../../elements/Button';
 import Icon from '../../elements/Icon';
-import PhotoInput from '../../elements/inputs/PhotoInput';
 import SelectInput from '../../elements/inputs/SelectInput';
 import TextInput from '../../elements/inputs/TextInput';
 import Textarea from '../../elements/inputs/Textarea';
-import { categories, types } from '../../../../constants/categories';
+import { categories } from '../../../../constants/categories';
 
 interface ISubmitModalForm {
   closeModal: () => void;
@@ -15,23 +14,17 @@ interface ISubmitModalForm {
 const getOptions = item => ({ key: item.name, value: item.slug });
 
 const SubmitModalForm: React.SFC<ISubmitModalForm> = ({ closeModal }) => {
-  const typeOptions = [
-    { key: 'Select a type', value: '' },
-    ...types.map(getOptions),
-  ];
   const categoryOptions = [
-    { key: 'Select a type', value: '' },
+    { key: 'Select a category', value: '' },
     ...categories.map(getOptions),
   ];
   return (
     <>
-      <PhotoInput />
-      <Flex justifyContent="space-between" >
-        <SelectInput label="Type" name="type" mr={3} mb={3} options={typeOptions} />
-        <SelectInput label="Category" name="category" mb={3} options={categoryOptions} />
-      </Flex>
       <Flex justifyContent="space-between" >
         <TextInput label="Username" name="username" placeholder="@example" mr={3} mb={3} />
+        <SelectInput label="Category" name="category" mb={3} options={categoryOptions} />
+      </Flex>
+      <Flex width={1}>
         <TextInput label="Name" name="name" placeholder="Lorem Ipsum" mb={3} />
       </Flex>
       <Flex mb={4}>
