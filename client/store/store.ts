2bfbe850d81c103e5778d69e2abc2c7162676faf
Pouria Ezzati <ezzati.upt@gmail.com>
2018-08-19 15:15:33 +0430

    Add redux-form reducer and store

--- client/store/store.ts
+++ client/store/store.ts
@@ -1,14 +1,17 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { FormStateMap, reducer as formReducer } from 'redux-form';
 import { authReducer, AuthStateTypes, IAuthState } from './auth';
 
 export interface IAppState {
   auth: IAuthState;
+  form: FormStateMap;
 }
 
 const rootReducer = combineReducers<IAppState>({
   auth: authReducer,
+  form: formReducer,
 });
 
 const store = (state: IAppState, action) => {
