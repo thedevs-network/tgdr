0b4694626cd2b15579e5db30d517de9c4555b8ba
Pouria Ezzati <ezzati.upt@gmail.com>
2018-09-04 00:04:59 +0430

    Extend error to create custom error constructor

---
+++ server/helpers/customError.ts
@@ -0,0 +1,13 @@
+class CustomError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+    if (typeof Error.captureStackTrace === 'function') {
+      Error.captureStackTrace(this, this.constructor);
+    } else { 
+      this.stack = (new Error(message)).stack; 
+    }
+  }
+}
+
+export default CustomError;
