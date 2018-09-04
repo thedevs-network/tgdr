66314d02063d76f4b8c033c621b450a31bca701a
Pouria Ezzati <ezzati.upt@gmail.com>
2018-09-04 23:26:34 +0430

    Add fetching members

--- server/controllers/botController.ts
+++ server/controllers/botController.ts
@@ -6,18 +6,27 @@ import CustomError from '../helpers/customError';
 
 export const getChatDetails = async (username: string) => {
   username = `@${username}`;
+
   try {
-    const details = await bot.telegram.getChat(username);
+    const [details, members] = await Promise.all([
+      bot.telegram.getChat(username), 
+      bot.telegram.getChatMembersCount(username),
+    ]);
+
     const { file_path } = await bot.telegram.getFile(
       details.photo.small_file_id
     );
+
     const image = 
       `https://api.telegram.org/file/bot${config.bot_token}/${file_path}`;
-    return { ...details, image };
+
+    return { ...details, image, members };
+
   } catch (error) {
     const message = error.code === 400 
       ? 'Chat not found.' 
       : 'An error occurred. Try again later.';
+
     throw new CustomError(message);
   }
 };
