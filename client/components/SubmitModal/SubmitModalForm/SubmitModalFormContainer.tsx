fa6566b40c90c2b18a14904fac8d93561441d4a1
Pouria Ezzati <ezzati.upt@gmail.com>
2018-09-07 17:24:25 +0430

    Use submit entry actions and state

--- client/components/SubmitModal/SubmitModalForm/SubmitModalFormContainer.tsx
+++ client/components/SubmitModal/SubmitModalForm/SubmitModalFormContainer.tsx
@@ -2,25 +2,51 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { InjectedFormProps, reduxForm } from 'redux-form';
 import SubmitModalForm from './SubmitModalForm';
+import { ISubmitEntryState, submitNewEntry } from '../../../store/submitEntry';
+import { Flex } from 'grid-styled';
+import Spinner from '../../elements/Spinner';
+import MessageModal from '../../MessageModal';
 
 interface IProps {
   closeModal: () => void;
+  submitEntry: ISubmitEntryState;
+  submitNewEntry: typeof submitNewEntry;
 }
 
 class SubmitModalFormContainer extends React.Component<
   IProps & InjectedFormProps<{}, IProps>
-  > {
+> {
   constructor(props) {
     super(props);
     this.submit = this.submit.bind(this);
   }
 
   submit(values) {
-    alert(JSON.stringify(values));
+    this.props.submitNewEntry(values);
   }
 
   render() {
-    const { closeModal, handleSubmit } = this.props;
+    const { closeModal, handleSubmit, submitEntry } = this.props;
+
+    if (submitEntry.isLoading) {
+      return (
+        <Flex justify="center" my={4}>
+          <Spinner size={32} />
+        </Flex>
+      );
+    }
+
+    if (submitEntry.isFetched) {
+      return (
+        <MessageModal
+          closeModal={closeModal}
+          text={submitEntry.message.text}
+          title={submitEntry.message.title}
+          type={submitEntry.message.type}
+        />
+      );
+    }
+
     return (
       <SubmitModalForm
         onSubmit={handleSubmit(this.submit)}
@@ -29,8 +55,6 @@ class SubmitModalFormContainer extends React.Component<
     );
   }
 }
-
-const mapStateToProps = ({ }) => ({});
 
 const validate = values => {
   const errors = {} as any;
@@ -49,34 +73,36 @@ const validate = values => {
   return errors;
 };
 
-const asyncValidate = values => new Promise((resolve, reject) => {
-  const errors = {} as any;
-  if (values.username && values.username.replace('@', '').length < 5) {
-    errors.username = 'Username must have at least 5 chars.';
-  }
-  if (
-    values.username && values.username.length > 1 &&
-    !/^@?[a-z]\w+$/gi.test(values.username)
-  ) {
-    errors.username = 'Only 0-9, a-z and underscores allowed.';
-  }
-  if (values.title && values.title.length < 3) {
-    errors.title = 'Title must have at least 3 chars.';
-  }
-  if (values.title && values.title.length > 54) {
-    errors.title = 'Too long. Title must have max 54 chars.';
-  }
-  if (values.description && values.description.length < 20) {
-    errors.description = 'Description must have at least 20 chars.';
-  }
-  if (values.description && values.description.length > 800) {
-    errors.description = 'Too long. Description must have max 800 chars.';
-  }
-  if (Object.keys(errors).length) {
-    return reject(errors);
-  }
-  resolve();
-});
+const asyncValidate = values =>
+  new Promise((resolve, reject) => {
+    const errors = {} as any;
+    if (values.username && values.username.replace('@', '').length < 5) {
+      errors.username = 'Username must have at least 5 chars.';
+    }
+    if (
+      values.username &&
+      values.username.length > 1 &&
+      !/^@?[a-z]\w+$/gi.test(values.username)
+    ) {
+      errors.username = 'Only 0-9, a-z and underscores allowed.';
+    }
+    if (values.title && values.title.length < 3) {
+      errors.title = 'Title must have at least 3 chars.';
+    }
+    if (values.title && values.title.length > 54) {
+      errors.title = 'Too long. Title must have max 54 chars.';
+    }
+    if (values.description && values.description.length < 20) {
+      errors.description = 'Description must have at least 20 chars.';
+    }
+    if (values.description && values.description.length > 800) {
+      errors.description = 'Too long. Description must have max 800 chars.';
+    }
+    if (Object.keys(errors).length) {
+      return reject(errors);
+    }
+    resolve();
+  });
 
 const SubmitForm = reduxForm<{}, IProps>({
   asyncBlurFields: ['username', 'title', 'description'],
@@ -85,4 +111,9 @@ const SubmitForm = reduxForm<{}, IProps>({
   validate,
 })(SubmitModalFormContainer);
 
-export default connect(mapStateToProps, null)(SubmitForm);
+const mapStateToProps = ({ submitEntry }) => ({ submitEntry });
+
+export default connect(
+  mapStateToProps,
+  { submitNewEntry }
+)(SubmitForm);
