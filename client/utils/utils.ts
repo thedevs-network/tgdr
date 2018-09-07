45558d31f466cceb11ecb4334831043764f04c30
Pouria Ezzati <ezzati.upt@gmail.com>
2018-09-07 22:11:37 +0430

    Add wait utility function

--- client/utils/utils.ts
+++ client/utils/utils.ts
@@ -1,2 +1,7 @@
 export const shortenLongName = (name: string, maxChars: number) =>
   name.length > maxChars ? `${name.slice(0, maxChars)}...` : name;
+
+export const wait = (ms: number) =>
+  new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
