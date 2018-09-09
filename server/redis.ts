74b04e0045b8e1db11d04364cee531232ee841fe
Pouria Ezzati <ezzati.upt@gmail.com>
2018-09-09 14:37:49 +0430

    Add redis

---
+++ server/redis.ts
@@ -0,0 +1,7 @@
+import { promisify } from 'util';
+import * as redis from 'redis';
+
+const client = redis.createClient();
+
+export const get = promisify(client.get).bind(client);
+export const set = promisify(client.set).bind(client);
