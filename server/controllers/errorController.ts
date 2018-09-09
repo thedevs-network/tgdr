e25b266aedb08a3bf0fbdee8f405a7fb1b42b806
Pouria Ezzati <ezzati.upt@gmail.com>
2018-09-09 14:39:56 +0430

    Use signale as error logger

--- server/controllers/errorController.ts
+++ server/controllers/errorController.ts
@@ -1,10 +1,14 @@
 import * as express from 'express';
+import * as signale from 'signale';
 import CustomError from '../helpers/customError';
+import config from '../config';
 
 export const send: express.ErrorRequestHandler = (error, _req, res, _next) => {
-  // If it's not a custom error message send a generic error message
+  // If it's not a custom error message use a generic error message
   const message =
     error instanceof CustomError ? error.message : 'An error occurred.';
+
+  if (config.is_dev) signale.fatal(error);
 
   res.status(422).json({ error: message });
 };
