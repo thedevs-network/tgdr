44beb6d2018e6555700c42267d3456a7ac1adf76
Pouria Ezzati <ezzati.upt@gmail.com>
2018-09-15 13:27:42 +0430

    Remove initial loading page

--- client/withVerifyToken.tsx
+++ client/withVerifyToken.tsx
@@ -3,20 +3,14 @@ import { NextSFC } from 'next';
 import { connect } from 'react-redux';
 import Cookie from 'js-cookie';
 import { renewToken } from './store/auth';
-import { AppLoadingWrapper } from './components/elements/Layout';
-import Spinner from '../client/components/elements/Spinner';
 
 interface IProps {
   isAuthenticated: boolean;
   renewToken: typeof renewToken;
 }
 
-interface IState {
-  isLoading: boolean;
-}
-
 const withVerifyToken = (Page: NextSFC) => {
-  class WithVerifyToken extends React.Component<IProps, IState> {
+  class WithVerifyToken extends React.Component<IProps> {
     static getInitialProps(ctx) {
       if (Page.getInitialProps) {
         return Page.getInitialProps(ctx);
@@ -24,36 +18,17 @@ const withVerifyToken = (Page: NextSFC) => {
       return {};
     }
 
-    state: IState;
-    constructor(props) {
-      super(props);
-      this.state = {
-        isLoading: true,
-      };
-    }
-
-    async componentDidMount() {
+    componentDidMount() {
       const { isAuthenticated, renewToken: renew } = this.props;
       if (isAuthenticated) return null;
 
       const token = Cookie.get('token');
       if (token) {
-        await renew(token);
+        renew(token);
       }
-      this.setState({ isLoading: false });
     }
 
     render() {
-      const { isLoading } = this.state;
-
-      if (isLoading) {
-        return (
-          <AppLoadingWrapper>
-            <Spinner size={36} />
-          </AppLoadingWrapper>
-        );
-      }
-
       return <Page {...this.props} />;
     }
   }
