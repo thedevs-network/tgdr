63771ccdfc0127c6269b287e83e0e29d91bb6e27
poeti8 <ezzati.upt@gmail.com>
2018-10-06 22:53:54 +0330

    Add get authentication header utility

--- client/utils/utils.ts
+++ client/utils/utils.ts
@@ -1,3 +1,14 @@
+import { IAppState } from '../store';
+
+export const getAuthHeader = (getState: () => IAppState) => {
+  const { token } = getState().auth;
+  return {
+    headers: {
+      Authorization: token,
+    },
+  };
+};
+
 export const shortenLongName = (name: string, maxChars: number) =>
   name.length > maxChars ? `${name.slice(0, maxChars)}...` : name;
 
