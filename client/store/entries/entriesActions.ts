023a01a99bfc2752c4f9d59c183e122ec215e3fe
poeti8 <ezzati.upt@gmail.com>
2018-10-21 12:30:03 +0330

    Check if params are equal to not call the API

--- client/store/entries/entriesActions.ts
+++ client/store/entries/entriesActions.ts
@@ -7,6 +7,7 @@ import {
   IGetEntriesResponse,
 } from './entriesTypes';
 import { AsyncAction } from '../storeTypes';
+import { areParamsEqual } from '../../utils';
 
 export const entriesRequest = (payload: IGetEntriesParams) =>
   action(EntriesStateTypes.REQUEST, payload);
@@ -17,13 +18,17 @@ export const entriesFailure = () => action(EntriesStateTypes.FAILURE);
 
 export const getEntries: AsyncAction = (
   params: IGetEntriesParams
-) => async dispatch => {
+) => async (dispatch, getState) => {
+  const { entries } = getState();
+
+  const paramsEqual = areParamsEqual(params, entries);
+  
+  if (paramsEqual) return null;
+  
   const query = queryString.stringify(params);
   dispatch(entriesRequest(params));
   try {
-    const {
-      data: { data },
-    } = await axios.get(`/api/etnry?${query}`);
+    const { data } = await axios.get(`/api/entry?${query}`);
     dispatch(entriesSuccess(data));
   } catch (error) {
     dispatch(entriesFailure());
--- client/utils/utils.ts
+++ client/utils/utils.ts
@@ -1,4 +1,5 @@
 import { IAppState } from '../store';
+import { IEntriesState, IGetEntriesParams } from '../store/entries';
 
 export const getAuthHeader = (getState: () => IAppState) => {
   const { token } = getState().auth;
@@ -12,7 +13,22 @@ export const getAuthHeader = (getState: () => IAppState) => {
 export const shortenLongName = (name: string, maxChars: number) =>
   name.length > maxChars ? `${name.slice(0, maxChars)}...` : name;
 
+export const capitalizeFirstLetter = (text: string): string =>
+  text[0].toUpperCase() + text.slice(1);
+
 export const wait = (ms: number) =>
   new Promise(resolve => {
     setTimeout(resolve, ms);
   });
+
+export const areParamsEqual = (
+  params: IGetEntriesParams,
+  state: IEntriesState
+): boolean =>
+  // Number of current entries should be same or more than requested
+  params.limit <= state.limit &&
+  state.data[params.type][params.sort].length &&
+  // If there are categories, they should be equal
+  (params.category || state.category
+    ? params.category === state.category
+    : true);
