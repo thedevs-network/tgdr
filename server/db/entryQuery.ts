102e8aff7b32b75d6640f5f59f5018a6c256d718
poeti8 <ezzati.upt@gmail.com>
2018-10-24 22:18:50 +0330

    Fix typo

--- server/db/entryQuery.ts
+++ server/db/entryQuery.ts
@@ -3,6 +3,6 @@ import { IEntryQuery } from '../types';
 
 export const find = (username: string) => Entry.findOne({ username });
 
-export const create = (etnry: IEntryModel) => new Entry(etnry).save();
+export const create = (entry: IEntryModel) => new Entry(entry).save();
 
 export const get = (query: IEntryQuery) => Entry.getEntries(query);
