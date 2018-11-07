5ca2c8361767129b78c78b198bdea3ea0502f58c
poeti8 <ezzati.upt@gmail.com>
2018-11-07 23:23:46 +0330

    Add report reason options

--- constants/texts.ts
+++ constants/texts.ts
@@ -56,3 +56,30 @@ export const getSubmitEntryMessages = (error?: string): IMessage => ({
     type: 'success',
   },
 });
+
+export const reportMessages: IMessage = {
+  error: {
+    text: "Couldn't submit the report. Try again later.",
+    title: 'Error',
+    type: 'error',
+  },
+  success: {
+    text: 'Submitted the report successfully.',
+    title: 'Reported',
+    type: 'success',
+  },
+}
+
+export const reportOptions: string[] = [
+  'Information is wrong',
+  'No longer exists',
+  'No longer working (bot)',
+  'Not English',
+  'Adult/Spam content',
+  'Other'
+];
+
+export const reportReviewOptions: string[] = [
+  'Spam/Advertisement',
+  'Not English',
+];
