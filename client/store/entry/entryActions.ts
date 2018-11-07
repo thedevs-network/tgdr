86a8de0e28f77b8322ed4fd96a8df1cc69d97800
poeti8 <ezzati.upt@gmail.com>
2018-11-07 23:23:35 +0330

    Add report action

--- client/store/entry/entryActions.ts
+++ client/store/entry/entryActions.ts
@@ -2,6 +2,7 @@ import { action } from 'typesafe-actions';
 import axios from 'axios';
 import { EntryStateTypes } from './entryTypes';
 import { AsyncAction, IEntry } from '../storeTypes';
+import { getAuthHeader } from '../../utils';
 
 export const entryRequest = () => action(EntryStateTypes.REQUEST);
 
@@ -10,14 +11,36 @@ export const entrySuccess = (payload: IEntry) =>
 
 export const entryFailure = () => action(EntryStateTypes.FAILURE);
 
-export const getEntry: AsyncAction = (username: string) => async dispatch => {
+export const getEntry: AsyncAction = (username: string) => async (
+  dispatch,
+  getState
+) => {
   dispatch(entryRequest());
   try {
     const {
       data: { data },
-    } = await axios.get(`/api/entry/${username.replace('@', '')}`);
+    } = await axios.get(
+      `/api/entry/${username.replace('@', '')}`,
+      getAuthHeader(getState)
+    );
     dispatch(entrySuccess(data));
   } catch (error) {
     dispatch(entryFailure());
   }
+};
+
+export const reportEntry: AsyncAction = (values: {
+  reason: string;
+  info?: string;
+}) => (_d, getState) => {
+  const {
+    data: { username },
+  } = getState().entry;
+
+  const data = {
+    ...values,
+    username,
+  };
+  
+  return axios.post('/api/entry/report', data, getAuthHeader(getState));
 };
