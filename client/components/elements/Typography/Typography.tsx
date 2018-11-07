564757f1ed58c96fe00f76ca7275fa7d19729370
poeti8 <ezzati.upt@gmail.com>
2018-11-07 23:23:27 +0330

    Add checkbox label and improve link

--- client/components/elements/Typography/Typography.tsx
+++ client/components/elements/Typography/Typography.tsx
@@ -33,13 +33,49 @@ interface ILabel {
 }
 
 export const Label = styled.label<ILabel>`
-  font-size: ${ifProp({ smallLabel: true }, '14px', '16px')};
+  font-size: ${ifProp('smallLabel', '14px', '16px')};
   font-weight: 500;
   margin-bottom: 8px;
   padding: 0;
 `;
 
+export const CheckboxLabel = styled.label`
+  padding-left: 16px;
+`;
+
 export const InputErrorMessage = styled.small`
   padding-top: 4px;
   color: #f44336;
+`;
+
+interface ILink {
+  big?: boolean;
+  secondary?: boolean;
+  small?: boolean;
+}
+
+export const Link = styled.a<ILink>`
+  font-size: ${ifProp('big', '16px', '14px')};
+  color: ${ifProp('secondary', '#C7CFD6', '#63b3f3')};
+  text-decoration: none;
+  transition: color 0.3s ease-out, transform 0.3s ease-out;
+  transform: translateZ(0);
+
+  ${ifProp(
+    'small',
+    css`
+      font-size: 13px;
+    `
+  )};
+
+  :hover,
+  :focus {
+    text-decoration: none;
+    color: #42a5f5;
+    transform: ${ifProp(
+      'secondary',
+      'translateZ(0)',
+      'scaleX(1.01) translateX(-4px)'
+    )};
+  }
 `;
