d7e135f5747cd0a20f4851d279f5ff1cfa194205
poeti8 <ezzati.upt@gmail.com>
2018-11-07 23:08:31 +0330

    Add reviewById handler and fix controllers data

--- server/controllers/reviewController.ts
+++ server/controllers/reviewController.ts
@@ -11,9 +11,21 @@ import {
 
 export const withReview: express.RequestHandler = async (req, res, next) => {
   const review = await reviewQuery.findOne({
-    entry: res.locals.entry,
-    user: req.user,
+    entry: res.locals.entry && res.locals.entry._id,
+    user: req.user && req.user._id,
   });
+
+  res.locals.review = review;
+
+  return next();
+};
+
+export const withReviewById: express.RequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  const review = await reviewQuery.findById(req.body.reviewId);
 
   res.locals.review = review;
 
@@ -35,31 +47,33 @@ export const create: express.RequestHandler = async (req, res) => {
 
   await reviewQuery.create({
     ...req.body,
+    created_at: new Date(),
     disliked: !!disliked,
     entry,
     liked: !!liked,
     user: req.user,
   });
 
-  const feedbacks = getEntryFeedback(review, disliked, liked);
-  const score = getScore(entry, feedbacks);
-  await entryQuery.update(username, { ...feedbacks, score });
+  if (!(review.liked === liked && review.disliked === disliked)) {
+    const feedbacks = getEntryFeedback(review, disliked, liked);
+    const score = getScore(entry, feedbacks);
+    await entryQuery.update(username, { ...feedbacks, score });
+  }
 
   return res
     .status(201)
     .json({ message: 'Review has been submitted successfully.' });
 };
 
-export const remove: express.RequestHandler = async (req, res) => {
-  const { review } = res.locals;
-  const { username } = req.body;
+export const remove: express.RequestHandler = async (_req, res) => {
+  const { entry, review } = res.locals;
 
   if (!review) throw new CustomError("Couldn't find the review");
 
   await reviewQuery.remove(review);
 
   const feedbackQuery = getEntryRemoveFeedback(review.liked);
-  await entryQuery.update(username, feedbackQuery);
+  await entryQuery.update(entry.username, feedbackQuery);
 
   return res
     .status(200)
