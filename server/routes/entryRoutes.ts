819766c3f42f7b2da4f30087131691f91d919d1b
poeti8 <ezzati.upt@gmail.com>
2018-11-07 23:10:28 +0330

    Add update and report routes

--- server/routes/entryRoutes.ts
+++ server/routes/entryRoutes.ts
@@ -1,15 +1,17 @@
 import * as express from 'express';
 import * as passport from 'passport';
 import * as asyncHandler from 'express-async-handler';
+import * as authController from '../controllers/authController';
+import * as botController from '../controllers/botController';
 import * as entryController from '../controllers/entryController';
 import * as reviewController from '../controllers/reviewController';
-import { entryValidator, newEntryValidators } from '../utils';
+import { entryValidator, newEntryValidators, reportValidator } from '../utils';
 import * as validatorsController from '../controllers/validatorsController';
 
 const router = express.Router();
 
 router.post(
-  '/submit',
+  '/',
   passport.authenticate('jwt', { session: false }),
   newEntryValidators,
   asyncHandler(validatorsController.checkForErrors),
@@ -19,15 +21,38 @@ router.post(
   asyncHandler(entryController.create)
 );
 
+router.put(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  asyncHandler(authController.adminCheck),
+  newEntryValidators,
+  asyncHandler(validatorsController.checkForErrors),
+  asyncHandler(entryController.getDetails),
+  asyncHandler(entryController.downloadImage),
+  asyncHandler(entryController.update)
+);
+
 router.get('/', asyncHandler(entryController.get));
 
 router.get(
   '/:username',
+  authController.optionalJwt,
   entryValidator,
   asyncHandler(validatorsController.checkForErrors),
   asyncHandler(entryController.withEntry),
   asyncHandler(reviewController.withReview),
   asyncHandler(entryController.getSingle)
+);
+
+
+router.post(
+  '/report',
+  passport.authenticate('jwt', { session: false }),
+  reportValidator,
+  asyncHandler(validatorsController.checkForErrors),
+  asyncHandler(entryController.withEntry),
+  asyncHandler(reviewController.withReviewById),
+  asyncHandler(botController.sendReport),
 );
 
 export default router;
