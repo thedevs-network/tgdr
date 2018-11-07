5a138a0c9b278981efdeff372cbb2cc1ccff1ecd
poeti8 <ezzati.upt@gmail.com>
2018-11-07 23:24:58 +0330

    Show real data on single entry page

--- pages/single.tsx
+++ pages/single.tsx
@@ -1,46 +1,80 @@
+import Button from '../client/components/elements/Button';
 import * as React from 'react';
-import { NextSFC } from 'next';
 import { Flex } from 'grid-styled';
-import { getEntry, IEntryState } from '../client/store/entry';
+import { getEntry } from '../client/store/entry';
 import withVerifyToken from '../client/withVerifyToken';
 import { INextContextWithRedux } from '../client/store';
 import Body from '../client/components/Body';
 import { LightBox } from '../client/components/elements/Layout';
-import { Description, Title } from '../client/components/elements/Typography';
+import {
+  Description,
+  Link,
+  Title,
+} from '../client/components/elements/Typography';
 import Image from '../client/components/elements/Image';
-import Button from '../client/components/elements/Button';
+import { NextSFC } from 'next';
 import Icon from '../client/components/elements/Icon';
 import Divider from '../client/components/elements/Divider';
 import Rate from '../client/components/Rate';
 import InfoList from '../client/components/InfoList';
 import ReviewForm from '../client/components/ReviewForm';
 import Reviews from '../client/components/Reviews';
+import { getReviews } from '../client/store/reviews';
+import { getOpenLink } from '../client/utils';
+import Modal from '../client/components/elements/Modal';
+import SubmitModal from '../client/components/SubmitModal';
+import { IEntry } from '../client/store/storeTypes';
+import ReportModal from '../client/components/ReportModal';
+import { IAuthState } from '../client/store/auth';
+import LoginModal from '../client/components/LoginModal';
 
 interface IProps {
-  entry: IEntryState;
+  auth: IAuthState;
+  entry: IEntry;
 }
 
-const Single: NextSFC<IProps> = ({ entry: { data } }) => {
-  const openLink = () => {
-    window.open(`https://t.me/${data.username}`, '_blank');
-    window.focus();
-  };
-
+const Single: NextSFC<IProps> = ({ entry, auth }) => {
+  const report = auth.isAuthenticated ? (
+    closeModal => <ReportModal closeModal={closeModal} />
+  ) : (
+    closeModal => <LoginModal closeModal={closeModal} />
+  );
+  
   return (
     <Body>
       <LightBox p={4} flex="1 1 auto">
         <Flex width={[106]} mr={4} flexDirection="column">
-          <Image w={[106]} h={106} username={data.username} />
-          <InfoList entry={data} />
+          <Image w={[106]} h={106} username={entry.username} />
+          <InfoList entry={entry} />
+          <Modal
+            trigger={
+              <Link href="#" title="Report" secondary small>
+                Report
+              </Link>
+            }
+          >
+            {report}
+          </Modal>
+          {auth.isAdmin && (
+            <Modal
+              trigger={
+                <Link href="#" title="Edit" secondary small>
+                  Edit
+                </Link>
+              }
+            >
+              {closeModal => <SubmitModal closeModal={closeModal} isEdit />}
+            </Modal>
+          )}
         </Flex>
         <Flex flexDirection="column" flex="1 1 0">
           <Flex align="flex-start">
             <Flex flexDirection="column" mr={4} flex="1 1 0">
-              <Title>{data.title}</Title>
-              <Description>{data.description}</Description>
+              <Title>{entry.title}</Title>
+              <Description>{entry.description}</Description>
             </Flex>
             <Flex width={[116]} align="center" flexDirection="column">
-              <Button big onClick={openLink}>
+              <Button big onClick={getOpenLink(entry.username)}>
                 + Add
                 <Icon name="telegram" size={14} fill="#ffffff" ml={2} />
               </Button>
@@ -60,9 +94,12 @@ Single.getInitialProps = async ({
   reduxStore,
   query: { username },
 }: INextContextWithRedux) => {
-  await reduxStore.dispatch(getEntry(username));
-  const { entry } = reduxStore.getState();
-  return { entry };
+  await Promise.all([
+    reduxStore.dispatch(getEntry(username)),
+    reduxStore.dispatch(getReviews(username)),
+  ]);
+  const { auth, entry } = reduxStore.getState();
+  return { entry: entry.data, auth };
 };
 
 export default withVerifyToken(Single);
