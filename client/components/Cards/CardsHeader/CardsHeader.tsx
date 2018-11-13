dd1c3caa8b6dc1c82e2cfeba6f4823b5f631d77a
poeti8 <ezzati.upt@gmail.com>
2018-11-14 01:00:07 +0330

    Fix and improve linking

--- client/components/Cards/CardsHeader/CardsHeader.tsx
+++ client/components/Cards/CardsHeader/CardsHeader.tsx
@@ -7,31 +7,12 @@ import Icon from '../../elements/Icon';
 import * as categories from '../../../../constants/categories';
 import { IGetEntriesParams } from '../../../store/entries';
 import { getViewMoreLinkURL } from '../../../utils';
+import { ViewAllLink } from '../../elements/Typography';
 
 const Title = styled.h3`
   font-size: 18px;
   font-weight: 400;
   color: #85919f;
-`;
-
-const ViewAllLink = styled.a`
-  display: flex;
-  align-items: center;
-  font-size: 14px;
-  color: #64b5f6;
-  text-decoration: none;
-  transition: color 0.3s ease-out, transform 0.3s ease-out;
-  transform: translateZ(0);
-
-  :hover,
-  :focus {
-    color: #42a5f5;
-    transform: scaleX(1.01) translateX(-4px);
-
-    svg path {
-      fill: #42a5f5;
-    }
-  }
 `;
 
 interface IProps extends IGetEntriesParams {
@@ -70,8 +51,8 @@ const CardsHeader: React.SFC<IProps> = ({
       </Flex>
       <Flex align="center">
         {showViewAllLink && (
-          <Link href={`/${query}`} as={`/${linkURL}`} passHref scroll={false}>
-            <ViewAllLink href="#" title={`View all ${type}s`}>
+          <Link href={`/?${query}`} as={`/${linkURL}`} passHref scroll={false}>
+            <ViewAllLink title={`View all ${type}s`}>
               <span>View all</span>
               <Icon name="arrowRight" size={10} fill="#64B5F6" ml={10} />
             </ViewAllLink>
