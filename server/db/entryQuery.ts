7adcebe788a4aa6d13e718cb788410100e148f0f
poeti8 <ezzati.upt@gmail.com>
2018-11-14 01:15:31 +0330

    Refactor and typing

--- server/db/entryQuery.ts
+++ server/db/entryQuery.ts
@@ -1,15 +1,19 @@
+import { Document } from 'mongoose';
 import Entry, { IEntryModel } from '../models/Entry';
 import { IEntryQuery } from '../types';
 import { getEntryUpdates } from '../utils';
 
 export const find = (username: string) => Entry.findOne({ username }).lean();
 
-export const create = (entry: IEntryModel) => new Entry(entry).save();
+export const findById = (entry: string | Document) =>
+  Entry.findById(entry).lean();
+
+export const create = (entry: IEntryModel) => Entry.create(entry);
 
 export const update = async (
   username: string,
   body: Record<string, string | number | boolean>,
-  isAdmin = false,
+  isAdmin = false
 ) => {
   const updates = getEntryUpdates(body, isAdmin);
   await Entry.updateOne({ username }, updates, {
