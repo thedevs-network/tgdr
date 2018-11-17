9761b8da53c58f16ff9969f10022773b4a8a13ac
poeti8 <ezzati.upt@gmail.com>
2018-11-17 13:13:59 +0330

    Add query to update user likes and dislikes

--- server/db/authQuery.ts
+++ server/db/authQuery.ts
@@ -1,11 +1,21 @@
-import User from '../models/User';
-import { IUserQuery } from '../types';
+import User, { IUserModel } from '../models/User';
+import { getFeedbackUpdates } from '../utils';
 
 export const find = (telegram_id: number) => User.findOne({ telegram_id });
 
-export const create = (telegram_id: number, userData: IUserQuery) =>
+export const create = (telegram_id: number, userData: Partial<IUserModel>) =>
   User.findOneAndUpdate({ telegram_id }, userData, {
     new: true,
     setDefaultsOnInsert: true,
     upsert: true,
   });
+
+export const updateLikes = async (
+  telegram_id: number,
+  userData: Partial<IUserModel>
+) => {
+  const updates = getFeedbackUpdates(userData);
+  return User.findOneAndUpdate({ telegram_id }, updates, {
+    new: true,
+  }).lean();
+};
