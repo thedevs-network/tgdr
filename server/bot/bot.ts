03b2dda1c514c880fbdbb60c20669c2ae7e014ac
poeti8 <ezzati.upt@gmail.com>
2018-12-02 18:25:47 +0330

    Add nre rating commands

--- server/bot/bot.ts
+++ server/bot/bot.ts
@@ -3,15 +3,38 @@ import config from '../config';
 import asyncHandler from './asyncHandler';
 import * as auth from './auth';
 import * as commands from './commands';
+import * as rate from './rate';
 
 const bot = new Telegraf(config.bot_token);
 
+bot.use(asyncHandler(auth.onlyPrivate));
 bot.use(asyncHandler(auth.register));
 bot.use(asyncHandler(auth.adminCheck));
-bot.use(asyncHandler(auth.setCommandingFlag(true)));
-bot.command('ban', auth.authAdmin, asyncHandler(commands.ban));
-bot.command('unban', auth.authAdmin, asyncHandler(commands.unban));
-bot.use(asyncHandler(auth.setCommandingFlag(false)));
+
+bot.start(asyncHandler(commands.start));
+bot.hears(['💙', '👎'], asyncHandler(rate.likeDislike));
+bot.command('/skip', asyncHandler(rate.submit));
+bot.command('/rate', asyncHandler(rate.start));
+bot.on('text', asyncHandler(rate.reviewText));
+
+bot.use(asyncHandler(auth.clear));
+
+bot.help(commands.help);
+
+bot.command(
+  'ban',
+  auth.authAdmin,
+  auth.setCommandingFlag(true),
+  asyncHandler(commands.ban),
+  auth.setCommandingFlag(false)
+);
+bot.command(
+  'unban',
+  auth.authAdmin,
+  auth.setCommandingFlag(true),
+  asyncHandler(commands.unban),
+  auth.setCommandingFlag(false)
+);
 
 bot.startPolling();
 
