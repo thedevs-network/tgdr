efc3061acf63bb8934e21f1bd5f454439f27a521
Akash <jakash1997@gmail.com>
2018-12-14 22:16:35 +0530

    fix-1

--- server/offline/sw.ts
+++ server/offline/sw.ts
@@ -1,19 +1,19 @@
 // This is the "Offline copy of pages" service worker
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install',(event: any) => {
     const indexPage = new Request('/');
     event.waitUntil(
-      fetch(indexPage).then( (response) => {
+      fetch(indexPage).then((response) => {
         return caches.open('tgdr-offline').then((cache) => {
           return cache.put(indexPage, response);
         });
     }));
   });
   
-  self.addEventListener('fetch', (event) => {
+  self.addEventListener('fetch', (event: any) => {
     const updateCache = (request) => {
       return caches.open('tgdr-offline').then((cache) => {
-        return fetch(request).then(function (response) {
+        return fetch(request).then((response) => {
           return cache.put(request, response);
         });
       });
@@ -22,9 +22,9 @@ self.addEventListener('install', function(event) {
     event.waitUntil(updateCache(event.request));
   
     event.respondWith(
-      fetch(event.request).catch(function(error) {
+      fetch(event.request).catch((_error) => {
         return caches.open('tgdr-offline').then((cache) => {
-          return cache.match(event.request).then((matching) => {
+          return cache.match(event.request).then((matching: any) => {
             const report =  !matching || matching.status === 404?Promise.reject('no-match'): matching;
             return report
           });
